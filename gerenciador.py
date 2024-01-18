4b7400fbf790bbfcc063048e5456279fb39e845d
Priscila Clarindo <luizgustavo.frota@outlook.com>
2024-01-18 17:56:08 -0300

    Modulo 1 _ aula 2

--- gerenciador.py
+++ gerenciador.py
@@ -1,22 +1,35 @@
-def add_task(tarefas: list[str], nome_tarefa: str) -> None:
-    tarefa = {nome_tarefa: nome_tarefa, "completed": False}
-    tarefas.append(tarefa)
+def add_task(tarefas: list[str], task_name: str) -> None:
+    task = {"task": task_name, "completed": False}
+    tarefas.append(task)
     return
 
-tarefas = []
+def see_tasks(tasks: list[str]) -> None:
+    print("\nTask List:")
+    for count, task in enumerate(tasks, start = 1):
+        task_name = task['task']
+        status = "✅" if task["completed"] else " "
+        print(f"{count}. [{status}] {task_name}")
+    return
+
+
+
+
+tasks = []
 while True:
-    print("\n Menu do Gerenciador de Lista de tarefas:")
-    print("1. Adicionar tarefa")
-    print("2. Ver tarefas")
-    print("3. Atualizar Tarefa")
-    print("4. Completar Tarefa")
-    print("5. Deletar tarefas completadas")
-    print("6. Sair")
+    print("\n Task Manager Menu:")
+    print("1. Add task")
+    print("2. See taks")
+    print("3. Update Task")
+    print("4. Complete Tasks")
+    print("5. Delete all completed tasks")
+    print("6. Quit")
 
 
-    user_choice = input("Digite a sua escolha: ")
+    user_choice = input("Choose an option: ")
     if user_choice == "1":
         task_name = input("Digite o nome da tarefa que gostaria de adicionar: ")
-        add_task(tarefas, task_name)
+        add_task(tasks, task_name)
+    elif user_choice == "2":
+        see_tasks(tasks)
     elif user_choice == "6":
         break
